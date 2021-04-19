000e9b96ac00bf655198f99209a11904f7ebc697
Divakar Raut <divakar.raut@service.nsw.gov.au>
2021-04-19 21:10:07 +1000

    category models built

--- server.js
+++ server.js
@@ -1,63 +1,55 @@
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from "express"
-
+import express from "express";
 const app = express();
 
 import cors from "cors";
-
-import morgan from "morgan"
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(morgan("tiny"));
-app.use(express.urlencoded({ extended: false }))
- 
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
 // parse application/json
-app.use(express.json())
+app.use(express.json());
 
-import mongoClient from './config/db.js'
-
+import mongoClient from "./config/db.js";
 mongoClient();
- 
-// Load Routers
 
-import loginRouter from './routers/login.router.js'
-import productRouter from  './routers/product.router.js'
-
-
-
+// LOAD ROUTERS
+import loginRouter from "./routers/login.router.js";
+import categoryRouter from "./routers/category.router.js";
+import productRouter from "./routers/product.router.js";
 //USE APIS
+app.use("/api/v1/login", loginRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/product", productRouter);
 
-app.use('/api/v1/login', loginRouter)
-app.use("/api/v1/product", productRouter)
-
-app.get("/", function(req,res) {
-    res.send("Hello There");
+app.get("/", (req, res) => {
+	res.send("Hello World");
 });
 
-// 404 Return
+//404 return
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
+	const error = new Error("Resources not found");
+	error.status = 404;
 
-    const error = new Error("Resource not found")
-    error.status = 404
+	next(error);
+});
 
-    next(error)
-})
+//handle error
+import { handleError } from "./utils/errorHandler.js";
+app.use((error, req, res, next) => {
+	handleError(error, res);
+});
 
-// Error Handling
-import {handleError} from './utils/errorHandler.js'
+app.listen(PORT, error => {
+	if (error) console.log(error);
 
-
-app.use((error,req,res,next) => {
-    handleError(error,res)
-})
-
-app.listen(PORT, error =>{
-    if (error) console.log(erro)
-
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+	console.log(`Server is runn at http://localhost:${PORT}`);
+});
